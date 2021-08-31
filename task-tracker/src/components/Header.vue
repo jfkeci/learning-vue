36152858eeef8c9b8327da000b9281434282c650
filip <ja.lip132@gmail.com>
2021-08-31 13:53:56 +0200

    conditional

--- task-tracker/src/components/Header.vue
+++ task-tracker/src/components/Header.vue
@@ -2,6 +2,7 @@
   <header>
     <h1>{{ title }}</h1>
     <Button
+      v-show="homePage"
       @btn-click="$emit('toggle-add-task')"
       :text="showAddTask ? 'Close' : 'Add task'"
       :color="showAddTask ? 'red' : 'green'"
@@ -20,6 +21,15 @@ export default {
   props: {
     title: String,
     showAddTask: Boolean,
+  },
+  computed: {
+    homePage() {
+      if (this.$route.path === "/") {
+        return true;
+      } else {
+        return false;
+      }
+    },
   },
 };
 </script>
